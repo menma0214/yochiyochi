4994081f417d3d0cf51eb266fc0b436e5d054e3d
menma0214 <116738944+menma0214@users.noreply.github.com>
2024-06-08 06:25:26 +0900

    create facility page

---
+++ app/models/facility.rb
@@ -0,0 +1,9 @@
+class Facility < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    ["address", "business_hours", "contact", "created_at", "environment", "facility_url", "fee", "furigana", "id", "id_value", "image", "movie", "name", "request", "target_age", "title", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+end
