6bb8176cc77c6fe696aadb652da5e0b29aa7dc61
menma0214 <116738944+menma0214@users.noreply.github.com>
2024-06-08 13:21:55 +0900

    fix config.hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,5 +94,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.hosts<<"still-refuge-07948"
+  config.hosts<<"shielded-woodland-80823-2b94006a6d3f.herokuapp.com"
 end
