42c851175a755dc9f2aec97a97966b36cd5d5c95
menma0214 <116738944+menma0214@users.noreply.github.com>
2024-06-10 00:39:05 +0900

    fix scss

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,6 +1,7 @@
 class TopsController < ApplicationController
   skip_before_action :require_login
   def index
-    @facilities = Facility.all
+    @q = Facility.ransack(params[:q])
+    @facilities = @q.result(distinct: true).order(created_at: :desc).page(params[:page]).per(20)
   end
 end
