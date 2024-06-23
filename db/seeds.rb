88cb30b2b537ecff3ca49f810f2d151f9e770899
menma0214 <116738944+menma0214@users.noreply.github.com>
2024-06-24 00:01:37 +0900

    add facility_url

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Facility.create(title: '楽しいサンプル施設', name: '某所１', furigana: 'ボウショ', address: '〇〇県〇〇市〇〇１−１− 某子どもセンター', business_hours: '9:00~17:00', target_age: '0歳〜3歳', environment: '屋内', request: '近くに駐車場有', contact: 'TEL:000-000-0000', facility_url: 'xxx.example.com')
+Facility.create(title: '楽しいサンプル施設', name: '某所１', furigana: 'ボウショ', address: '〇〇県〇〇市〇〇１−１− 某子どもセンター', business_hours: '9:00~17:00', fee: '無料', target_age: '0歳〜3歳', environment: '屋内', request: '近くに駐車場有', contact: 'TEL:000-000-0000', facility_url: 'xxx.example.com')
 PlaygroundEquipment.create(facility_id: 1, title: 'サンプル小さな子供向けの遊具があるよ', kind: '滑り台 玩具 本', target_age: '0歳〜3歳', installation: '滑り台２台/玩具、本複数', remarks: 'はいはいを始めたぐらいのお子様から遊べます！')
