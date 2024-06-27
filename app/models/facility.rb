d605fdc58f18537866935540548ba7ec0062aee8
menma0214 <116738944+menma0214@users.noreply.github.com>
2024-06-27 15:28:28 +0900

    connection to S3

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -2,8 +2,11 @@ class Facility < ApplicationRecord
   has_many :playground_equipments
   has_many :reviews
 
+  mount_uploader :image, ImageUploader
+  mount_uploader :video, VideoUploader
+
   def self.ransackable_attributes(auth_object = nil)
-    ["address", "business_hours", "contact", "created_at", "environment", "facility_url", "fee", "furigana", "id", "id_value", "image", "movie", "name", "request", "target_age", "title", "updated_at"]
+    ["address", "business_hours", "contact", "created_at", "environment", "facility_url", "fee", "furigana", "id", "id_value", "image", "video", "name", "request", "target_age", "title", "updated_at"]
   end
 
   def self.ransackable_associations(auth_object = nil)
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,14 @@
+require 'carrierwave/orm/activerecord'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider:              'AWS',
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region:                'ap-southeast-1', # 自分が設定したいリージョン(Renderのリージョンの関係でシンガポール)
+  }
+  config.fog_directory  = ENV['AWS_S3_BUCKET']
+  config.fog_attributes = {'x-amz-acl' => nil} # ACLを設定しない
+end
