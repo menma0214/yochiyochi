66ec7307ebd63d01a6e4ddaa4c940b3b8f5596c6
menma0214 <116738944+menma0214@users.noreply.github.com>
2024-07-09 18:24:37 +0900

    １ページあたりの表示数を変更

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,6 +2,6 @@ class TopsController < ApplicationController
   skip_before_action :require_login
   def index
     @q = Facility.ransack(params[:q])
-    @facilities = @q.result(distinct: true).order(created_at: :desc).page(params[:page]).per(20)
+    @facilities = @q.result(distinct: true).order(created_at: :desc).page(params[:page]).per(5)
   end
 end
