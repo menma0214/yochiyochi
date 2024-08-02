e6a393d98cb9a57e608d6d4fcc4a81862ef608b8
menma0214 <116738944+menma0214@users.noreply.github.com>
2024-08-03 01:47:03 +0900

    fix precompile

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -1,5 +1,3 @@
-# Rails.application.config.dartsass.builds = {
-#   'application.scss' => 'application.css',
-#   'admin/application.scss' => 'admin/application.css',
-#   'admin/login.scss' => 'admin/login.css'
-# }
+Rails.application.config.dartsass.builds = {
+  'application.scss' => 'application.css',
+}
