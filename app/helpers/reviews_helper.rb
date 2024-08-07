ce85adbef40e6398ee55fc05df06c0720f62626d
menma0214 <116738944+menma0214@users.noreply.github.com>
2024-08-07 18:47:23 +0900

    add_star

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,20 @@
 module ReviewsHelper
+  def display_stars(rate)
+    rate ||= 0  # rateがnilの場合は0にする
+    full_star = '<i class="fas fa-star"></i>'
+    half_star = '<i class="fas fa-star-half-alt"></i>'
+    empty_star = '<i class="far fa-star"></i>'
+
+    stars = ''
+    5.times do |i|
+      if rate >= i + 1
+        stars += full_star
+      elsif rate >= i + 0.5
+        stars += half_star
+      else
+        stars += empty_star
+      end
+    end
+    stars.html_safe
+  end
 end
