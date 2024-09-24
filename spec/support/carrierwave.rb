d4f6aeddc591325e9e3242ce95175c040abb556b
menma0214 <116738944+menma0214@users.noreply.github.com>
2024-09-24 15:35:41 +0900

    fix frozunerror

--- spec/support/carrierwave.rb
+++ spec/support/carrierwave.rb
@@ -1,14 +1,20 @@
-CarrierWave.configure do |config|
-  if Rails.env.test? || Rails.env.cucumber?
+if Rails.env.test?
+  CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = false
-  else
-    config.storage = :fog
   end
-end
 
-RSpec.configure do |config|
-  config.after(:suite) do
-    FileUtils.rm_rf(Dir["#{Rails.root}/public/uploads"])
+  # モデルのアップローダーを無効化
+  CarrierWave::Uploader::Base.descendants.each do |klass|
+    next if klass.anonymous?
+    klass.class_eval do
+      def cache_dir
+        "#{Rails.root}/spec/support/uploads/tmp"
+      end
+
+      def store_dir
+        "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      end
+    end
   end
 end
