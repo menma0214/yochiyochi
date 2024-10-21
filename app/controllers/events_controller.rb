5d4e612a2b5e3582b2584c72d45cac46cf7051f3
menma0214 <116738944+menma0214@users.noreply.github.com>
2024-10-21 23:51:46 +0900

    add tag search and search option

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,6 +9,27 @@ class EventsController < ApplicationController
       @events = @q.result(distinct:true).order(created_at: :desc).page(params[:page]).per(5)
     end
 
+    # 大人料金での検索
+    if params[:price_range].present?
+      min_price, max_price = params[:adult_fee_range].split('-').map(&:to_i)
+      @events = @events.where(adult_fee: min_price..max_price)
+    else
+      @events = @q.result(distinct: true).order(created_at: :desc).page(params[:page]).per(5)
+    end
+
+    # 子供料金での検索
+    if params[:child_fee_range].present?
+      min_price, max_price = params[:child_fee_range].split('-').map(&:to_i)
+      @events = @events.where(child_fee: min_price..max_price)
+    end
+
+    # 住所カラムに都道府県が含まれているか確認
+    if params[:prefecture].present?
+      @events = @events.where('address LIKE ?', "%#{params[:prefecture]}%")
+    else
+      @events = @q.result(distinct: true).order(created_at: :desc).page(params[:page]).per(5)
+    end
+
     # タグの一覧を取得
     @tags = Tag.all
   end
